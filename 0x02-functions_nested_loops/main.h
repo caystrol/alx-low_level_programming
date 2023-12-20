2da053147d16470c74b1032b1c02877dc2b56d6b
caystrol <furahambugua@gmail.com>
2023-12-21 00:28:27 +0300

    readme

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* _putchar - writes the character c to stdout */
 int _putchar(char c);
+
+/* print_alphabet - prints the alphabet in lowercase followed by a new line */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
