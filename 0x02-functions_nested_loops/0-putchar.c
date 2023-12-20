4bda3cc38b41586cbd70e1ec1dc3b208b2568942
caystrol <furahambugua@gmail.com>
2023-12-20 14:26:18 +0300

    readme

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,5 @@
 #include "main.h"
 
-/* Function that prints _putchar followed by a new line */
-void print_putchar(void)
-{
-    char str[] = "_putchar\n";
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
