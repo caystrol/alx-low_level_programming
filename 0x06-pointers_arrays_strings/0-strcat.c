fd55db46a9b0612cc2a788f9c50efa89195d801a
caystrol <furahambugua@gmail.com>
2024-01-05 15:44:23 +0300

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to append
+ *
+ * Return: Pointer to the resulting string `dest`
+ */
+char *_strcat(char *dest, char *src)
+{
+    char *temp = dest;
+
+    while (*temp)
+        temp++;
+
+    while (*src)
+    {
+        *temp = *src;
+        temp++;
+        src++;
+    }
+
+    *temp = '\0'; /* Adding the null terminator at the end */
+
+    return dest;
+}
