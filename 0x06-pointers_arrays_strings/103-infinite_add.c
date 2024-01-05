5c4fe8708d59c5e36ea4230869e17d1531038ac7
caystrol <furahambugua@gmail.com>
2024-01-05 23:49:27 +0300

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+    int len1 = 0, len2 = 0, carry = 0, sum = 0, i = 0;
+    while (n1[len1])
+        len1++;
+    while (n2[len2])
+        len2++;
+    if (len1 + 2 > size_r || len2 + 2 > size_r)
+        return 0;
+    len1--;
+    len2--;
+    r[size_r - 1] = '\0';
+    for (i = size_r - 2; len1 >= 0 || len2 >= 0 || carry; i--) {
+        sum = carry;
+        if (len1 >= 0)
+            sum += n1[len1--] - '0';
+        if (len2 >= 0)
+            sum += n2[len2--] - '0';
+        carry = sum / 10;
+        r[i] = (sum % 10) + '0';
+    }
+    if (i < 0)
+        return 0;
+    return &r[i + 1];
+}
