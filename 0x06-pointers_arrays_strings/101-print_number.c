47f51cd7bf04240d52c2a11d58410f7c6d83a265
caystrol <furahambugua@gmail.com>
2024-01-05 23:37:04 +0300

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+    int div = 1;
+
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    while (n / div >= 10)
+        div *= 10;
+
+    while (div != 0) {
+        _putchar((n / div) % 10 + '0');
+        div /= 10;
+    }
+}
