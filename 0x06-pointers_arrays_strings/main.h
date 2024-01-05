7a11a301e6a158787b7159ddf60b6c5a161c964a
caystrol <furahambugua@gmail.com>
2024-01-05 16:10:48 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
-char *_strcat(char *dest, char *src);
+/* Function prototypes */
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
